44305842840e5eaec5097c3fa5b1861d02192f4e
mashii <mashii@yeah.net>
2017-03-24 12:24:46 +0000

    update

--- vim/vim/update.sh
+++ vim/vim/update.sh
@@ -2,13 +2,13 @@
 
 cd bundle/ && rm * -rf
 
-git clone https://github.com/VundleVim/Vundle.vim.git
-git clone https://github.com/kien/ctrlp.vim.git
-git clone https://github.com/vim-scripts/a.vim.git
-git clone https://github.com/easymotion/vim-easymotion.git
-git clone https://github.com/majutsushi/tagbar.git
-git clone https://github.com/godlygeek/tabular.git
-git clone https://github.com/mbbill/undotree.git
-git clone https://github.com/terryma/vim-multiple-cursors.git
-git clone https://github.com/SirVer/ultisnips.git
-git clone https://github.com/Yggdroot/vim-mark.git
+rm Vundle.vim -rf           && git clone https://github.com/VundleVim/Vundle.vim.git
+rm ctrlp.vim -rf            && git clone https://github.com/kien/ctrlp.vim.git
+rm a.vim -rf                && git clone https://github.com/vim-scripts/a.vim.git
+rm vim-easymotion -rf       && git clone https://github.com/easymotion/vim-easymotion.git
+rm tagbar -rf               && git clone https://github.com/majutsushi/tagbar.git
+rm tabular -rf              && git clone https://github.com/godlygeek/tabular.git
+rm undotree -rf             && git clone https://github.com/mbbill/undotree.git
+rm vim-multiple-cursors -rf && git clone https://github.com/terryma/vim-multiple-cursors.git
+rm ultisnips -rf            && git clone https://github.com/SirVer/ultisnips.git
+rm vim-mark -rf             && git clone https://github.com/Yggdroot/vim-mark.git
